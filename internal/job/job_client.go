ebcc55bf962d9094ce2a3a10bdbd1972843968d2
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-06-22 18:22:01 +0900

    delete GetDesiredMinMaxReplicas from jobClient

--- internal/job/job_client.go
+++ internal/job/job_client.go
@@ -17,7 +17,6 @@ import (
 type JobClient interface {
 	Start(ctx context.Context)
 	Stop()
-	GetDesiredMinMaxReplicas() apiv1.TemporaryScaleMetricSpec
 }
 
 type jobClient struct {
@@ -167,10 +166,6 @@ func (j *jobClient) updateStatus(ctx context.Context) error {
 	current.Status.DesiredMinReplicas = *j.desiredTemporaryScaleMetricSpec.MinReplicas
 	current.Status.DesiredMaxReplicas = j.desiredTemporaryScaleMetricSpec.MaxReplicas
 	return j.ctrlClient.Status().Update(ctx, &current)
-}
-
-func (j *jobClient) GetDesiredMinMaxReplicas() apiv1.TemporaryScaleMetricSpec {
-	return j.desiredTemporaryScaleMetricSpec
 }
 
 func (j *jobClient) Start(ctx context.Context) {
