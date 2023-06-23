940d8b6ae1cb90b37a3fffac5ae3e3807a787779
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-06-23 16:03:08 +0900

    rename jobClient to metricsJobClient

--- internal/metrics/fake_metrics_job_client.go
+++ internal/metrics/fake_metrics_job_client.go
@@ -7,26 +7,26 @@ import (
 	apiv1 "sample.com/custom-horizontal-pod-autoscaler/api/v1"
 )
 
-type FakeJobClient struct {
+type FakeMetricsJobClient struct {
 	desiredTemporaryScaleMetricSpec apiv1.TemporaryScaleMetricSpec
 }
 
-// Guarantee *FakeJobClient implements JobClient.
-var _ JobClient = (*FakeJobClient)(nil)
+// Guarantee *FakeMetricsJobClient implements JobClient.
+var _ MetricsJobClient = (*FakeMetricsJobClient)(nil)
 
-func FakeNew(desiredTemporaryScaleMetricSpec apiv1.TemporaryScaleMetricSpec) *FakeJobClient {
-	return &FakeJobClient{
+func FakeNew(desiredTemporaryScaleMetricSpec apiv1.TemporaryScaleMetricSpec) *FakeMetricsJobClient {
+	return &FakeMetricsJobClient{
 		desiredTemporaryScaleMetricSpec: desiredTemporaryScaleMetricSpec,
 	}
 }
 
-func (j *FakeJobClient) Start(ctx context.Context) {
+func (j *FakeMetricsJobClient) Start(ctx context.Context) {
 }
 
-func (c *FakeJobClient) Stop() {
+func (c *FakeMetricsJobClient) Stop() {
 }
 
-func (j *FakeJobClient) GetDesiredMinMaxReplicas() apiv1.TemporaryScaleMetricSpec {
+func (j *FakeMetricsJobClient) GetDesiredMinMaxReplicas() apiv1.TemporaryScaleMetricSpec {
 	return apiv1.TemporaryScaleMetricSpec{
 		MinReplicas: pointer.Int32(1),
 		MaxReplicas: int32(5),
