ed2081abed3f38359de99df9706710783b6ba7f1
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-06-25 15:52:06 +0900

    add test template

--- internal/metrics/metrics_collector_test.go
+++ internal/metrics/metrics_collector_test.go
@@ -1,1 +1,24 @@
 package metrics
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("MetricsJobClient", func() {
+	ctx := context.Background()
+
+	It("Should get persedQueryResult", func() {
+
+	})
+
+	BeforeEach(func() {
+
+	})
+
+	AfterEach(func() {
+		time.Sleep(100 * time.Millisecond)
+	})
+})
