d17426617c52a253fbf6d547aaf7230246e742a6
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-07-15 16:49:52 +0900

    set pusher option

--- internal/metrics-pusher/metrics-pusher.go
+++ internal/metrics-pusher/metrics-pusher.go
@@ -6,8 +6,14 @@ type MetricsPusher interface {
 type metricsPusher struct {
 }
 
-func NewPusher() (MetricsPusher, error) {
-	p := metricsPusher{}
+type PusherOption func(*metricsPusher)
+
+func NewPusher(opts ...PusherOption) (MetricsPusher, error) {
+	p := &metricsPusher{}
+
+	for _, opt := range opts {
+		opt(p)
+	}
 
 	return p, nil
 }
