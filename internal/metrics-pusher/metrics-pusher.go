2745a028b4aad67ff3b0fd0f87727fdcd3cbf01b
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-07-22 11:29:51 +0900

    create collector status vector

--- internal/metrics-pusher/metrics-pusher.go
+++ internal/metrics-pusher/metrics-pusher.go
@@ -11,8 +11,9 @@ type MetricsPusher interface {
 }
 
 type metricsPusher struct {
-	syncerTotal     *prometheus.GaugeVec
-	collectorStatus customautoscalingv1alpha1.CollectorStatus
+	syncerTotal        *prometheus.GaugeVec
+	collectorNotReady  *prometheus.GaugeVec
+	collectorAvailable *prometheus.GaugeVec
 }
 
 type PusherOption func(*metricsPusher)
@@ -27,6 +28,20 @@ func NewPusher(opts ...PusherOption) (MetricsPusher, error) {
 	}, []string{"controller"})
 	metrics.Registry.MustRegister(p.syncerTotal)
 
+	p.collectorNotReady = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "customhpa",
+		Name:      "collector_notready",
+		Help:      "The controller status about not ready condition",
+	}, []string{"controller", "name", "namespace"})
+	metrics.Registry.MustRegister(p.collectorNotReady)
+
+	p.collectorAvailable = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "customhpa",
+		Name:      "collector_notready",
+		Help:      "The controller status about available condition",
+	}, []string{"controller", "name", "namespace"})
+	metrics.Registry.MustRegister(p.collectorAvailable)
+
 	for _, opt := range opts {
 		opt(p)
 	}
@@ -36,4 +51,15 @@ func NewPusher(opts ...PusherOption) (MetricsPusher, error) {
 
 func (p *metricsPusher) SetSyncerTotal(count float64) {
 	p.syncerTotal.WithLabelValues("customhorizontalpodautoscaler").Set(count)
+}
+
+func (p *metricsPusher) SetCollectorStatus(namespace string, name string, status customautoscalingv1alpha1.CollectorStatus) {
+	switch status {
+	case customautoscalingv1alpha1.CollectorNotReady:
+		p.collectorNotReady.WithLabelValues("customhorizontalpodautoscaler", name, namespace).Set(1)
+		p.collectorAvailable.WithLabelValues("customhorizontalpodautoscaler", name, namespace).Set(0)
+	case customautoscalingv1alpha1.CollectorAvailable:
+		p.collectorNotReady.WithLabelValues("customhorizontalpodautoscaler", name, namespace).Set(0)
+		p.collectorAvailable.WithLabelValues("customhorizontalpodautoscaler", name, namespace).Set(1)
+	}
 }
