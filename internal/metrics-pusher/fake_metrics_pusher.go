908c446342942ae4ef76c24ed2955bd67e346370
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-07-22 16:41:35 +0900

    set fake metrics pusher

--- internal/metrics-pusher/fake_metrics_pusher.go
+++ internal/metrics-pusher/fake_metrics_pusher.go
@@ -1,1 +1,23 @@
 package pusher
+
+import (
+	customautoscalingv1alpha1 "github.com/TomoyukiSugiyama/custom-horizontal-pod-autoscaler/api/v1alpha1"
+)
+
+type FakeMetricsPusher struct {
+}
+
+// Guarantee *FakeMetricsPusher implements MetricsPusher.
+var _ MetricsPusher = (*FakeMetricsPusher)(nil)
+
+func FakeNewCollector() *FakeMetricsPusher {
+	return &FakeMetricsPusher{}
+}
+
+func (p *FakeMetricsPusher) SetSyncerTotal(count float64) {
+
+}
+
+func (p *FakeMetricsPusher) SetCollectorStatus(namespace string, name string, status customautoscalingv1alpha1.CollectorStatus) {
+
+}
