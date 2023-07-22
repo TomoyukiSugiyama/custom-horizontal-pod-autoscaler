fe369cca88e9efea01e63193b83be6b00f025078
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-07-22 19:11:44 +0900

    add test for collectorNotReady

--- internal/metrics-pusher/metrics_pusher_test.go
+++ internal/metrics-pusher/metrics_pusher_test.go
@@ -4,16 +4,18 @@ import (
 	"strings"
 	"time"
 
+	customautoscalingv1alpha1 "github.com/TomoyukiSugiyama/custom-horizontal-pod-autoscaler/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/prometheus/client_golang/prometheus/testutil"
 )
 
 var _ = Describe("Metrics Pusher", func() {
-	var pusher MetricsPusher
-	var err error
+	pusher, err := NewPusher()
+	// var err error
 	It("Should get syncerTotal", func() {
-
+		// pusher, err := NewPusher()
+		// Expect(err).NotTo(HaveOccurred())
 		pusher.SetSyncerTotal(1)
 
 		const metadata = `
@@ -24,18 +26,43 @@ var _ = Describe("Metrics Pusher", func() {
 
 		customhpa_syncer_total{controller="customhorizontalpodautoscaler"} 1
 		`
-		err := testutil.CollectAndCompare(pusher.GetSyncerTotal(), strings.NewReader(metadata+expected), "customhpa_syncer_total")
+		err = testutil.CollectAndCompare(pusher.GetSyncerTotal(), strings.NewReader(metadata+expected), "customhpa_syncer_total")
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	BeforeEach(func() {
-		pusher, err = NewPusher()
+	It("Should get collectorNotReady", func() {
+		pusher.SetCollectorStatus("test-ns", "test-custom-hpa", customautoscalingv1alpha1.CollectorNotReady)
+
+		const metadataNotReady = `
+		# HELP customhpa_collector_notready The controller status about not ready condition
+		# TYPE customhpa_collector_notready gauge
+		`
+		expectedNotReady := `
+
+		customhpa_collector_notready{controller="customhorizontalpodautoscaler",name="test-custom-hpa",namespace="test-ns"} 1
+		`
+		err = testutil.CollectAndCompare(pusher.GetCollectorNotReady(), strings.NewReader(metadataNotReady+expectedNotReady), "customhpa_collector_notready")
 		Expect(err).NotTo(HaveOccurred())
 
+		const metadataAvailable = `
+		# HELP customhpa_collector_available The controller status about available condition
+		# TYPE customhpa_collector_available gauge
+		`
+		expectedAvailable := `
+
+		customhpa_collector_available{controller="customhorizontalpodautoscaler",name="test-custom-hpa",namespace="test-ns"} 0
+		`
+		err = testutil.CollectAndCompare(pusher.GetCollectorAvailable(), strings.NewReader(metadataAvailable+expectedAvailable), "customhpa_collector_notready")
+		Expect(err).NotTo(HaveOccurred())
+
+	})
+
+	BeforeEach(func() {
 		time.Sleep(100 * time.Millisecond)
 	})
 
 	AfterEach(func() {
+
 		time.Sleep(100 * time.Millisecond)
 	})
 })
